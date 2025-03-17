1208efb14d65b77b79b0e030f91ef26bb6a8ed72
midCenturyDude7 <mgriffes@gmail.com>
2025-03-16 21:20:36 -0700

    Small edit

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t-[1px] border-slate-300/20 bg-[#2A3765]/30 shadow-2xl shadow-slate-100">
+    <footer className="border-t-[1px] border-slate-300/20 bg-[#2A3765]/10 shadow-2xl shadow-slate-100">
       <div className="p-8">Footer</div>
     </footer>
   );
--- app/components/Main/Main.jsx
+++ app/components/Main/Main.jsx
@@ -1,3 +1,7 @@
 export default function Main({ children }) {
-  return <div className="flex flex-1 flex-col px-8">{children}</div>;
+  return (
+    <div className="flex min-h-[100%] flex-1 flex-col justify-center px-8">
+      {children}
+    </div>
+  );
 }
