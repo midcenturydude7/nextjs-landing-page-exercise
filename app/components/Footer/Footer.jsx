826fff6f522c3825a668a2ea64b855cfcb70099d
midCenturyDude7 <mgriffes@gmail.com>
2025-03-20 22:42:25 -0700

    Updated config files and styles

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t-[1px] border-slate-300/20 bg-[#2A3765]/10 shadow-2xl shadow-slate-100">
+    <footer className="border-t-[1px] border-slate-300/20 bg-[#07091e]/50 shadow-2xl shadow-slate-50/50">
       <div className="p-8">Footer</div>
     </footer>
   );
