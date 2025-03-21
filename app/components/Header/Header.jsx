6b82eb78f84eb2d4ebb640e2d7c1343fc7fc9176
midCenturyDude7 <mgriffes@gmail.com>
2025-03-20 22:22:29 -0700

    Added Logo and MobileNav components

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,7 +1,11 @@
+import Logo from "./Logo/Logo";
+import MobileNav from "./MobileNav/MobileNav";
+
 export default function Header() {
   return (
-    <div className="flex border-b-[1px] border-slate-300/10 p-8 shadow-2xl">
-      <h1 className="text-xl font-bold">Header</h1>
-    </div>
+    <header className="flex border-b-[1px] border-slate-300/10 p-8 shadow-2xl">
+      <Logo />
+      <MobileNav />
+    </header>
   );
 }
