840cc2d430e98b9083abbe91b45d41e8ed0a1273
midCenturyDude7 <mgriffes@gmail.com>
2025-03-21 21:52:48 -0700

    Setting up scaffolding/structure for mobile navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+// This is a Tailwind CSS configuration file that specifies the content sources for Tailwind to scan for class names,
+// extends the default theme with custom background gradients, and includes any necessary plugins.
+// It is used to customize the styling of a web application built with React and Tailwind CSS.
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
