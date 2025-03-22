7202baba5b7a0e14ed04560915be649dccf691f8
midCenturyDude7 <mgriffes@gmail.com>
2025-03-22 12:03:13 -0700

    Adjusted bg color for curve to match menu

--- app/components/Header/MobileNav/Curve/Curve.jsx
+++ app/components/Header/MobileNav/Curve/Curve.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { curve } from "../../../../lib/anim";
 export default function Curve() {
   return (
-    <svg className="absolute -left-[99px] top-0 h-full w-[100px] fill-slate-300 stroke-none">
+    <svg className="absolute -left-[99px] top-0 h-full w-[100px] fill-slate-100 stroke-none">
       <motion.path
         variants={curve}
         initial="initial"
