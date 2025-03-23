2b2b78789d9968aca98ea8bbf28a8320aa2341d8
midCenturyDude7 <mgriffes@gmail.com>
2025-03-23 11:19:29 -0700

    Updated icon color scheme

--- app/components/Header/Icons/Icons.jsx
+++ app/components/Header/Icons/Icons.jsx
@@ -3,13 +3,13 @@ export default function Icons() {
     <div className="hidden lg:block">
       <ul className="flex items-center justify-center space-x-4">
         <li>
-          <div className="h-7 w-7 rounded-lg bg-slate-600/80" />
+          <div className="h-7 w-7 rounded-lg bg-slate-300/70" />
         </li>
         <li>
-          <div className="h-7 w-7 rounded-lg bg-slate-600/80" />
+          <div className="h-7 w-7 rounded-lg bg-slate-300/70" />
         </li>
         <li>
-          <div className="h-7 w-7 rounded-lg bg-slate-600/80" />
+          <div className="h-7 w-7 rounded-lg bg-slate-300/70" />
         </li>
       </ul>
     </div>
