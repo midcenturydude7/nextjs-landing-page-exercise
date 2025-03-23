def2ec49c8a4e71de55a8a4e6020a15707137ffb
midCenturyDude7 <mgriffes@gmail.com>
2025-03-22 22:02:03 -0700

    Added responsive icons section to header

---
+++ app/components/Header/Icons/Icons.jsx
@@ -0,0 +1,17 @@
+export default function Icons() {
+  return (
+    <div className="hidden lg:block">
+      <ul className="flex items-center justify-center space-x-4">
+        <li>
+          <div className="h-7 w-7 rounded-lg bg-slate-600/80" />
+        </li>
+        <li>
+          <div className="h-7 w-7 rounded-lg bg-slate-600/80" />
+        </li>
+        <li>
+          <div className="h-7 w-7 rounded-lg bg-slate-600/80" />
+        </li>
+      </ul>
+    </div>
+  );
+}
