c6d1804a06182681900da0ba1f8d1fdcba4d51c2
midCenturyDude7 <mgriffes@gmail.com>
2025-03-25 22:00:31 -0700

    Set up the rest of the routes

---
+++ app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-4xl font-bold">
+        Landing Page Exercise - Contact Page
+      </h1>
+    </div>
+  );
+}
