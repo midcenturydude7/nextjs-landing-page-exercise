279843e347385bdc9182628fd1835584cbe679a2
midCenturyDude7 <mgriffes@gmail.com>
2025-03-25 21:57:48 -0700

    Set up routes with about page

---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-4xl font-bold">Landing Page Exercise - About Page</h1>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
-      <h1 className="text-4xl font-bold">Landing Page Exercise</h1>
+      <h1 className="text-4xl font-bold">Landing Page Exercise - Home Page</h1>
     </div>
   );
 }
